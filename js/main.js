b4e422425cffb77a69b8069c0c7723a332f06cac
mohamedelajour <mohamedelajour@gmail.com>
2024-08-11 03:13:30 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// alert("هل بتحب تقراء يسطا")
+
+
+
+document.getElementsByClassName("div_fixed_a")[7].onclick=function(){
+    document.getElementsByClassName("log_in")[0].style.visibility= "visible";
+}
+document.getElementsByClassName("div_fixed_a")[0].onclick=function(){
+    document.getElementsByClassName("log_in")[0].style.visibility= "hidden";
+}
+document.getElementsByClassName("button_home2")[0].onclick=function(){
+    document.getElementsByClassName("h2_home")[0].style.color="blue";
+}
+document.getElementsByClassName("button_home2")[0].onclick=function(){
+    document.getElementsByClassName("works0")[0];
+    document.scrollingElement("works0")[0];
+}
+
+
+
